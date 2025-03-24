675ed1ddd2e01ee87a4607bdd5e1a33fc9802bbd
marta <marta.garzena@gmail.com>
2025-03-24 10:57:15 +0100

    modifiche lezione 24-03

--- controller.py
+++ controller.py
@@ -1,7 +1,68 @@
 from view import View
 from model import Model
 import flet as ft
+
+
 class Controller(object):
     def __init__(self, view: View):
         self._view = view
         self._model = Model()
+
+    def getNMax(self):
+        return self._model.NMax
+
+    def getTMax(self):
+        return self._model.TMax
+
+    def reset(self, event):
+        self._model.reset()
+        self._view._txtOutT.value = self._model.T
+        self._view._lv.controls.clear()
+        self._view._btnPlay.disabled = False
+        self._view._txtIn.disabled = False
+        self._view._lv.controls.append(ft.Text("Indovina il numeros!!"))
+        self._view.update()
+
+    def play(self, event):
+
+
+        tentativoStr = self._view._txtIn.value #è una string
+
+        self._view._txtIn.value = ""
+        self._view._txtOutT.value = self._model.T-1
+
+        if tentativoStr == "":
+            self._view._lv.controls.append(ft.Text("Inserisci qualcosa su", color="red"))
+            self._view.update()
+            return
+
+        try:
+            tentativo = int(tentativoStr)
+        except ValueError:
+            self._view._lv.controls.append(ft.Text("Inserisci un NUMEROO", color="red"))
+            self._view.update()
+            return
+
+        res = self._model.play(tentativo) #0 se vinco, 2 se vite finite, 1 se segreto piu grande, -1 se segreto piu piccolo
+        if res == 0:
+            self._view._lv.controls.append(ft.Text(f"Hai vinto, il numero era {tentativoStr}", color="green"))
+            self._view._btnPlay.disabled = True
+            self._view._txtIn.disabled = True
+            self._view.update()
+            return
+        elif res == 2:
+            self._view._lv.controls.append(ft.Text(f"Hai perso, vite finite. Il numero era: {self._model.segreto}", color="red"))
+            self._view._btnPlay.disabled = True
+            self._view._txtIn.disabled = True
+            self._view.update()
+            return
+        elif res == 1:
+            self._view._lv.controls.append(ft.Text(f"Il numero segreto è piu grande di {tentativoStr}", color="orange"))
+            self._view.update()
+            return
+        elif res == -1:
+            self._view._lv.controls.append(ft.Text(f"Il numero segreto è piu piccolo di {tentativoStr}", color="orange"))
+            self._view.update()
+            return
+
+        self._view.update()
--- model.py
+++ model.py
@@ -1,5 +1,57 @@
 import random
 
+
+#contiene la logica del gioco
 class Model(object):
     def __init__(self):
-        pass
+        self._NMax = 100
+        self._TMax = 6  #numero di vite
+        self._T = self._TMax
+        #self.NMin = 1
+        self._segreto = None
+        #random.randint(self.NMin, self.NMAx))
+
+    def reset(self):
+        self._segreto = random.randint(0, self._NMax)  #numero casuale
+        self._T = self._TMax  #resetta le vite del gioco
+        print("segreto: ", self._segreto)
+
+    def play(self, guess):
+        """funzione che segue un tentativo
+        :param guess:int
+        :return: 0 se vinco, 2 se vite finite, 1 se segreto piu grande, -1 se segreto piu piccolo"""
+        # ricevo un tentatico e lo confronto con _segreto
+
+        self._T -= 1
+        if guess == self._segreto:
+            return 0  #vittoria
+        if self._T == 0:
+            return 2  #sconfitta per numero di vite
+        #se arrivo qui, ho ancora vite. Il tentativo va valutato
+        if guess > self._segreto:
+            return -1  #il segreto è più piccolo guess
+        else:
+            return 1  #il segreto è più grande del guess
+
+    @property
+    def NMax(self):
+        return self._NMax
+
+    @property
+    def TMax(self):
+        return self._TMax
+
+    @property
+    def T(self):
+        return self._T
+
+    @property
+    def segreto(self):
+        return self._segreto
+
+
+if __name__ == "__main__":
+    m = Model()
+    m.reset()
+    print(m.play(50))
+    print(m.play(95))
